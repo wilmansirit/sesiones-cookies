06176e39f7784446c1fbec694c7556fdb27f2ee0
wilmansirit <wsirit@gmail.com>
2019-04-28 14:20:05 -0400

    Lista para su futuro uso

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 'use strict'
 
+/* PERSISTENCIA DE SESIONES CON MONGODB, MySQL
+Uso de mongodb, mysql para el almacenamiento de sesiones
+
+Ver más en: 
+	https: //www.youtube.com/watch?v=DS-TST4FW2k
+	http: //www.cleverclouds.im/es/blog/2018/06/guardar-la-sesi%C3%B3n-en-mysql-para-el-framework-express-en-node
+	*/
+
 const 	express 	= require('express')
 	,	session		= require('express-session')
 	,	MongoStore	= require('connect-mongo')(session)				// Para almacenar las sesiones con mogodb

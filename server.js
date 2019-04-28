61930011a93d6eef06f07ddffb33fd235753ae73
wilmansirit <wsirit@gmail.com>
2019-04-28 14:07:21 -0400

    Conf express-session with express-mysql-session

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 
 const 	express 	= require('express')
 	,	session		= require('express-session')
-	,	MongoStore	= require('connect-mongo')(session)
+	,	MongoStore	= require('connect-mongo')(session)				// Para almacenar las sesiones con mogodb
+	,	MySQLStore	= require('express-mysql-session')(session)		// Para Almacenar las Sesiones con MySQL
 
+require('dotenv').config()
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/sesiones-cookies'
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/sesiones-cookies'
+
 const app = express();
 
 app.use(session({
@@ -13,9 +16,16 @@ app.use(session({
 	resave				:	true,					// Forzar a cada llama de sesion se guarde en base de datos haya cambios o no
 	saveUninitialized	:	true,					// Cada session es un objeto donde se le agregan propiedades que contiene información de la session
 													// (quien es el usuario, etc.). Al iniciar está vacío y con true forzamos a guardarlo en base de datos
-	store				:	new MongoStore({		// Para almacenar en base de datos las sesiones 
-		url				:	MONGO_URL,				// Nombre de la base de datos a almacenar las sesiones
-		autoReconnect	:	true					// Auto reconexión en caso de fallar
+	// store				:	new MongoStore({		// Para almacenar en base de datos las sesiones 
+	// 	url				:	MONGO_URL,				// Nombre de la base de datos a almacenar las sesiones
+	// 	autoReconnect	:	true					// Auto reconexión en caso de fallar
+	// })
+	store				:	new MySQLStore({		// Uso de MySQL para el almacenamiento de las sesiones
+		host			:	'localhost',
+		port			:	3306,
+		user			:	process.env.LOCAL_USERNAME,
+		password		:	process.env.LOCAL_PASSWORD,
+		database		:	process.env.LOCAL_DATABASE
 	})
 }))
 
@@ -23,8 +33,6 @@ app.get('/', (req, res) => {
 	req.session.cuenta = req.session.cuenta ? req.session.cuenta + 1 : 1;
 	res.send(`Hola!. Has visto esta página ${req.session.cuenta}`)
 })
-
-
 
 
 
